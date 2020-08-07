8715f6e90a4557fcac62465e4b120f99ea8055f1
ivy <unjust@gmail.com>
2020-08-06 19:04:50 -0500

    other stuff for the map demo

--- App.js
+++ App.js
@@ -2,9 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { Dimensions, Platform, StyleSheet, Text, View, TouchableOpacity, Switch, Image } from 'react-native';
 import * as Permissions from 'expo-permissions';
-import MapView, { PROVIDER_GOOGLE, Marker, Polyline, Animated, AnimatedRegion } from 'react-native-maps';
+import MapView, { UrlTile, LocalTile, PROVIDER_GOOGLE, Marker, Polyline, Animated, AnimatedRegion, Overlay } from 'react-native-maps';
 // import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import haversine from 'haversine';
+import { mapStyle1, mapStyle2 } from './mapStyle';
+import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
+
+//const tilesPath = `${docDir}/tiles/tiles/{z}_{x}_{y}.png`;
+const imm = require("./tiles/tile01.png")
+const imageURI = Asset.fromModule(require("./tiles/tile01.png")).uri;
 
 export default class App extends Component  {
   constructor(props) {
@@ -32,7 +39,7 @@ export default class App extends Component  {
   }
 
   getMapRegion = () => {
-    console.log(this.state);
+    // console.log(this.state);
     return new AnimatedRegion({
       latitude: this.state.latitude,
       longitude: this.state.longitude,
@@ -82,7 +89,7 @@ export default class App extends Component  {
         } else {
           // coordinate.timing(newCoordinate).start();
         }
-
+        console.log(imm, imageURI);
         this.setState({
           latitude,
           longitude,
@@ -97,7 +104,6 @@ export default class App extends Component  {
   }
 
   render() {
-    console.log("yoooo", this.state.coordinate);
     return (
       <View style={styles.container}>
         <Text>GoctaLapp</Text>
@@ -106,20 +112,28 @@ export default class App extends Component  {
           showsUserLocation
           followsUserLocation
           loadingEnabled
-          mapType="hybrid"
+          //mapType="hybrid"
           // or initialRegion?
+          mapType={Platform.OS == "android" ? "none" : "standard"}
           region={this.getMapRegion()}
           provider={PROVIDER_GOOGLE}
           style={styles.mapStyle} 
+          customMapStyle={mapStyle1}
         >
-          <Polyline
+          {/* <Polyline
             coordinates={this.state.routeCoordinates}
             strokeWidth={5}
             strokeColor="#FFF" />
           <Marker.Animated ref={ marker => { this.marker = marker; } }
             // coordinate={{ latitude: -16, longitude: -70 }}
             coordinate={ this.state.coordinate }
-          />
+          /> */}
+           {/* https://gitmemory.com/issue/react-native-community/react-native-maps/2088/471427151 */}
+          {/* <UrlTile urlTemplate={imageURI} zIndex={-1}  /> */}
+          <Overlay 
+            image={imageURI}
+            //image="https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg"
+            bounds={[[-6.05, -77.89],[-6.06, -77.92]]} />
         </MapView.Animated>
       </View>
     );
