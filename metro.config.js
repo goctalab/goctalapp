064531d0f7315746f1746003e07b3ae8707104e6
ivy <unjust@gmail.com>
2020-08-09 11:21:57 -0500

    add kml and xml to assets for expo

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+const defaultAssetExts = require("metro-config/src/defaults/defaults").assetExts;
+
+// https://stackoverflow.com/questions/55484740/how-to-append-extension-in-metro-config-js-for-metro-bundler
+module.exports = {
+    resolver: {
+        assetExts: [
+            ...defaultAssetExts,
+            'kml',
+            'xml'
+        ]
+    }
+};
