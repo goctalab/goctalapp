8543007d1f21cba6f777a1bc484f060fa4304807
ivy <unjust@gmail.com>
2020-08-18 18:50:17 -0500

    skip DS store processed

---
+++ src/mapData.js
@@ -0,0 +1,24 @@
+// export default KML_DATA = {
+//   "./assets/kml/TNQ1alto2ha.kml": require("./assets/kml/TNQ1alto2ha.kml"),
+//   "./assets/kml/GLperimetro.kml": require("./assets/kml/GLperimetro.kml"),
+//   "./assets/kml/PTOAGUA.kml": require("./assets/kml/PTOAGUA.kml")
+// }
+
+export const mapOverlayCoordinates = [
+  { 
+    longitude: -77.89747715238839,
+    latitude: -6.054255104565719
+  },
+  // { 
+  //   longitude: -77.89744399531243,
+  //   latitude: -6.054210153938867
+  // },
+  // { 
+  //   longitude: -77.89790378080357,
+  //   latitude: -6.055070647890809
+  // },
+  { 
+    longitude: -77.89676365107341,
+    latitude: -6.055672561546392
+  }
+];
