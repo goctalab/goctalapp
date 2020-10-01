f9c938c047d16868d27ac9261e58f4ac11d00ce0
ivy <unjust@gmail.com>
2020-09-30 19:31:17 -0500

    adding src to babel

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = function(api) {
         {
           'root': ['./'], 
           'alias': {
+            '@src': './src',
             '@assets': './assets',
             '@data': './src/data',
             '@components': './src/components'
