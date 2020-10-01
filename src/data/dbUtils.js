3b3f7cb18de590ecee319a8f50ed13aa2f703dbf
ivy <unjust@gmail.com>
2020-09-30 19:30:56 -0500

    adding await to try to quiet errors

--- src/data/dbUtils.js
+++ src/data/dbUtils.js
@@ -1,16 +1,17 @@
 import * as FileSystem from 'expo-file-system';
 import * as SQLite from 'expo-sqlite';
-import { AsyncStorage } from 'react-native';
+// import { AsyncStorage } from 'react-native';
 
-// SQLite.openDatabase(
-//   { name : "goctaTestDB", createFromLocation : '~db/gocta1'},
-//   () => console.log("ok db"),
-//   () => console.log("errors db"));
 const PLACES_DESCRIPTION_TABLE = "data2";
 const KML_TABLE = "kml";
 
 export default {
   db: null,
+  /**
+   * @method
+   * @description checks if the db exists and loads and opens the db file
+   * @returns Promise
+   */
   init: function() {
     FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite/goctaTest.db')
       .then(function(resp) {
@@ -33,42 +34,35 @@ export default {
   },
   error: (err) => { console.log(`received db error ${err}`); },
   getAllKML: function(callback) {
-    //return new Promise(function(resolve, reject) {
-     
-      return this.db.transaction(function (tx) {
-        // console.log("executing sql");
-        tx.executeSql(
-          `SELECT filename, coordinates, kml_type from ${KML_TABLE}`,
-          [], 
-          (tx, { rows }) => { 
-            // console.log('success', rows.length);
-            // console.table(rows._array);
-            callback(rows._array);
-          }, 
-          function(err) {
-            console.log(`some error ${err}`); 
-          });
-        }, 
-        this.error, 
-        () => { console.log('transaction completed'); })
-    // })
-  },
-  getAllPlaces: function() {
-    console.log("and places");
-    // data2 has kml_file title description type
-    // also make call to kkml to cross reference filename
-    return this.db.transaction(function (tx) {
-      tx.executeSql(
-        `SELECT * from ${PLACES_DESCRIPTION_TABLE}`,
+    return this.db.transaction(async function (tx) {
+      // console.log("executing sql");
+      await tx.executeSql(
+        `SELECT filename, coordinates, kml_type from ${KML_TABLE}`,
         [], 
-        (tx, { rows }) => { 
-          console.log("success", rows.length);
-          console.table(rows._array);
-          return rows._array;
+        (_, { rows }) => { 
+          callback(rows._array);
         }, 
-        function(err) {
-          debugger
-          console.log(`some error ${err}`); 
+        (err) => {
+          console.log(`error from getAllKML ${err}`); 
+        });
+      }, 
+      this.error, 
+      () => { console.log('transaction completed'); })
+  },
+  getAllPlaces: function(callback) {
+    // data2 has kml_file title description type
+    // also make call to kml to cross reference filename
+    return this.db.transaction(async function(tx) {
+      await tx.executeSql(
+        `SELECT title, kml_file, type from ${PLACES_DESCRIPTION_TABLE}`,
+        [], 
+        (_, { rows }) => { 
+          // console.log("success", rows.length);
+          // console.table(rows._array);
+          callback(rows._array);
+        }, 
+        (err) => {
+          console.log(`error from places sql ${err}`); 
         });
       }, 
       this.error, 
