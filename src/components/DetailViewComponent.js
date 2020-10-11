fe4ee37ff0ad5f5fb57a09217b4a5867ff84e939
ivy <unjust@gmail.com>
2020-10-10 22:05:59 -0500

    format description and add back button to detail view is from map

--- src/components/DetailViewComponent.js
+++ src/components/DetailViewComponent.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import dbUtils from '@data/dbUtils';
 import { HOME_ROUTE } from '@utils/routeUtils';
@@ -7,7 +7,7 @@ import { HOME_ROUTE } from '@utils/routeUtils';
 export default function DetailViewComponent({ route, navigation }) {
 
   const [ description, setDescription ] = useState("Loading...");
-  const { id, title, filename } = route.params;
+  const { id, title, filename, from_map } = route.params;
  
   // db call
   useEffect(() => {
@@ -17,14 +17,26 @@ export default function DetailViewComponent({ route, navigation }) {
     }, id);
   });
 
+  useLayoutEffect(() => {
+    if (!from_map) {
+      return;
+    }
+    navigation.setOptions({
+      headerLeft: () => (
+        <Button onPress={() => { navigation.navigate(HOME_ROUTE) }} title="Close" />
+      )
+    });
+  }, [route]);
+
+  const newLineDescription = (description.replace(/(?:\\\\[rn])+/g, ' \n'));
   return (
     <View style={{ flex: 1,
       alignItems: 'center',
-      justifyContent: 'top',
+      justifyContent: 'top',  
       padding: 30,
       paddingHorizontal: 50 }}>
       <Text style={{ fontSize: 25, marginBottom: 24, textAlign: 'center' }}>{title}</Text>
-      <Text style={{ fontSize: 18,  textAlign: 'justify' }}>{description}</Text>
+      <Text style={{ fontSize: 18 }}>{newLineDescription}</Text>
       <Button
         title="Go to map view"
         onPress={() => navigation.navigate(HOME_ROUTE, { selected_marker: filename } )} />
