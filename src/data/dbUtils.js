2c262b5524b263dca131e6eb37cf883a5bffd455
Ivy Feraco <unjust@gmail.com>
2020-11-06 21:28:35 -0500

    Web service, attempting download of db version if higher version is available (#17)
    * gitignore backup db
    
    * writing code to download db from endpoint
    
    * comments
    
    * readme
    
    * yarn lock
    
    * jest fetch mock and chaining promises in dbUtils, test passing
    
    * clearing up this bound, removing consoel log
    
    * get rid of fake fetch

--- src/data/dbUtils.js
+++ src/data/dbUtils.js
@@ -1,7 +1,5 @@
 import * as FileSystem from 'expo-file-system';
 import * as SQLite from 'expo-sqlite';
-import { Asset } from 'expo-asset';
-
 // const DB_NAME = process.env.DB_NAME || 'gocta';
 const PLACES_DESCRIPTION_TABLE = process.env.SITES_TABLE || 'sites';
 const KML_TABLE = process.env.KML_TABLE || 'kml ';
@@ -29,47 +27,50 @@ export const KML_FIELDS = {
 }
 
 const dbDir = FileSystem.documentDirectory + 'SQLite/';
-// Checks if gif directory exists. If not, creates it
-async function ensureDirExists() {
-  const dirInfo = await FileSystem.getInfoAsync(dbDir)
+const GOCTA_DB_FILENAME = "goctaTest.db";
+/**
+ * @description Checks if gif directory exists. If not, creates it
+ */
+export async function ensureDirExists() {
+  const dirInfo = await FileSystem.getInfoAsync(dbDir);
+  // console.log("dirinfo", dirInfo);
   if (!dirInfo.exists) {
     console.log('sqlite directory doesn\'t exist, creating...');
     await FileSystem.makeDirectoryAsync(dbDir, { intermediates: true });
     return;
   }
-  return;
+  return true;
 }
 
 export default {
   db: null,
+  current_db_version: null,
+  onUpdateDbFromRemote: () => {},
   /**
    * @method
    * @description checks if the db exists and loads and opens the db file
    * @returns Promise
    */
-  init: async function() {
+  setDB: function(dbFile) {
+    // https://forums.expo.io/t/how-to-connect-to-an-existing-database/6154
+    this.db = SQLite.openDatabase(dbFile, 0.1);
+  },
+
+  init: async function(updateCallback) {
     if (this.db) {
       return;
     }
 
-    const callback = (function({ uri }) {
-      const db = SQLite.openDatabase('goctaTest.db', 0.1);
-      // https://forums.expo.io/t/how-to-connect-to-an-existing-database/6154
-      // const db = SQLite.openDatabase(uri, 0.1);
-      console.log('db is at this uri', uri, db);
-      this.db = db;
-      return this;
-    }).bind(this);
+    this.onUpdateDbFromRemote = updateCallback;
 
     await ensureDirExists();
 
-    const module = require('@assets/db/gocta_test.db');
-  
-    console.log("module is", module);
-
     return FileSystem.downloadAsync(
-      Expo.Asset.fromModule(module).uri, `${dbDir}/goctaTest.db`)
-      .then(callback)
+      Expo.Asset.fromModule(require('@assets/db/gocta_test.db')).uri, `${dbDir}/${GOCTA_DB_FILENAME}`)
+      .then(() =>{
+        this.setDB(GOCTA_DB_FILENAME);
+        this.checkForUpdate();
+      })
       .catch((err) => {
         console.log("error when downloading db");
       });
@@ -77,9 +78,73 @@ export default {
 
   error: (err) => console.log(`received db error ${err}`),
 
+  getCurrentDbVersion: function() {
+    return new Promise((resolve, reject) => {
+      if (this.current_db_version) {
+        resolve(this.current_db_version);
+        return;
+      }
+      this.db.transaction(function (tx) {
+        tx.executeSql(
+          `PRAGMA user_version`,
+          [], 
+          (_tx, { rows }) => { 
+            this.current_db_version = rows._array[0].user_version;
+            // console.log(this.current_db_version, rows);
+            resolve(this.current_db_version);
+          }, 
+          (_tx, err) => {
+            console.log(`error from check for update ${err}`); 
+          });
+        }, 
+        () => { reject(err); }, 
+        () => { console.log('check for update transaction completed');
+      })
+    });
+  },
+
+  checkForUpdate: async function() {
+    const dbVersion = await this.getCurrentDbVersion();
+    return fetch('http://192.168.0.105/get_db_version')
+      .then((resp) => resp.text())
+      .then((remote_version) => {
+        // could be that app_db_version isnt set yet
+        // console.log('remote version is greater', remote_version, parseInt(remote_version) > dbVersion);
+        if (parseInt(remote_version) > dbVersion) {
+          console.log("will proceed with download...");
+          this.downloadUpdate();
+        }
+      })
+      .catch((err) => { 
+        console.log('error getting db version', err); 
+      });
+  },
+
+  downloadUpdate: function() {
+    const remoteDBFilename = "goctaTest_tmp.db";
+    return FileSystem.downloadAsync('http://192.168.0.105/get_db', `${dbDir}/${remoteDBFilename}`)
+      .then(() =>
+        FileSystem.moveAsync({ 
+          from: `${dbDir}/${GOCTA_DB_FILENAME}`,
+          to: `${dbDir}/goctaTest.db.prev`,
+        })
+      )
+      .then(() =>
+        FileSystem.moveAsync({ 
+          from: `${dbDir}/${remoteDBFilename}`,
+          to: `${dbDir}/${GOCTA_DB_FILENAME}`,
+        })
+      )
+      .then(() => {
+        this.setDB(GOCTA_DB_FILENAME);
+        this.onUpdateDbFromRemote();
+        // console.log('success update db is at this uri', uri, db);
+      })
+      .catch((err) => console.log(`moving async err ${err}`))
+  },
+  
   getAllKML: function(callback) {
     return this.db.transaction(async function (tx) {
-      // console.log("executing sql");
       await tx.executeSql(
         `SELECT ${ROW_ID}, ${KML_FIELDS.filename}, ${KML_FIELDS.coordinates}, ${KML_FIELDS.type} from ${KML_TABLE} ORDER BY ${ROW_ID} DESC`,
         [], 
@@ -96,14 +161,11 @@ export default {
   },
 
   getAllPlaces: function(callback) {
-    // data2 has kml_file title description type
-    // also make call to kml to cross reference filename
     return this.db.transaction(async function(tx) {
       await tx.executeSql(
         `SELECT ${ROW_ID}, ${PLACE_FIELDS.title}, ${PLACE_FIELDS.title}, ${PLACE_FIELDS.description}, ${PLACE_FIELDS.filename}, ${PLACE_FIELDS.category} from ${PLACES_DESCRIPTION_TABLE}`,
         [], 
         (_tx, { rows }) => { 
-          // console.log("success", rows.length);
           // console.table(rows._array);
           callback(rows._array);
         }, 
@@ -121,7 +183,6 @@ export default {
         `SELECT ${PLACE_FIELDS.title}, ${PLACE_FIELDS.description} from ${PLACES_DESCRIPTION_TABLE} where rowid = ?`,
         [id], 
         (_tx, { rows }) => { 
-          // console.log("success", rows.length);
           // console.table(rows._array);
           callback(rows._array[0]);
         },
---
+++ web_service/README.md
@@ -0,0 +1,4 @@
+These scripts are installed on the server at /var/www/html/gocta_api/
+and are responsible for the endpoints to update the db
+
+Using mod_wsgi to set up the endpoint in apache2 with python https://modwsgi.readthedocs.io/en/master/
